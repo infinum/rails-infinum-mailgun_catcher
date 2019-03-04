cb2454b492cf9511855b23d3bde6daecf6ca1267
Jaka Šušteršič <uncoverd@gmail.com>
2019-03-04 09:06:39 +0100

    Add configurable webhook controller path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 MailgunCatcher::Engine.routes.draw do
-  resources :webhooks, only: [:create]
+  resources :webhooks, only: [:create], path: MailgunCatcher::Engine.config.resource_name
 end
--- lib/mailgun_catcher/engine.rb
+++ lib/mailgun_catcher/engine.rb
@@ -1,5 +1,9 @@
 module MailgunCatcher
   class Engine < ::Rails::Engine
+    extend Dry::Configurable
+
+    setting :resource_name, 'webhooks'
+
     isolate_namespace MailgunCatcher
   end
 end
