ed2a63b8fd771b34635845879d143a701b710388
Vedran Hrncic <vedran.hrncic@infinum.hr>
2019-05-08 14:22:15 +0200

    add support for notifiers

--- lib/mailgun_catcher.rb
+++ lib/mailgun_catcher.rb
@@ -2,7 +2,14 @@ require 'dry-configurable'
 require 'mailgun_catcher/engine'
 require 'mailgun_catcher/event'
 require 'mailgun_catcher/event_error'
+require 'mailgun_catcher/notifier/bugsnag'
 
 module MailgunCatcher
-  # Your code goes here...
+  extend Dry::Configurable
+
+  setting(:notifier, MailgunCatcher::Notifier::Bugsnag.new)
+
+  def self.setup
+    yield config
+  end
 end
