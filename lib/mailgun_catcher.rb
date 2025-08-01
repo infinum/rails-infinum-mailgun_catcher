e7413dc36a145430e4b80506af92a829d541034d
Marko Ćilimković <marko.cilimkovic@infinum.com>
2025-08-01 15:18:44 +0200

    Remove dry-configurable as a dependency

--- lib/mailgun_catcher.rb
+++ lib/mailgun_catcher.rb
@@ -1,14 +1,21 @@
-require 'dry-configurable'
 require 'mailgun_catcher/engine'
 require 'mailgun_catcher/event'
 require 'mailgun_catcher/notifier/bugsnag'
 
 module MailgunCatcher
-  extend Dry::Configurable
-
-  setting(:notifiers, default: ["MailgunCatcher::Notifier::Bugsnag"])
-
   def self.setup
     yield config
+  end
+
+  def self.config
+    @config ||= Config.new(notifiers: ['MailgunCatcher::Notifier::Bugsnag'])
+  end
+
+  class Config
+    attr_accessor :notifiers
+
+    def initialize(notifiers:)
+      @notifiers = notifiers
+    end
   end
 end
